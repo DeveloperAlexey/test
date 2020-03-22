eb197d6b114cf2da3a4f8770c2da4e72c9d04ad9
Alexsey <dollar123dollar@mail.ru>
2020-03-22 08:11:45 +0200

    this first commit

---
+++ start.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var leftborderwidth = 3;
+let second = 2;
+const pi = 3.14;
+console.log(4/0);
+ console.log('string'*9);
+ let samthyng;
+ console.log(samthyng);
+let person = {
+    name: 'pasha',
+    age: 47,
+    maried: false
+};
+console.log(person.name);
+console.log(person["age"]);
+//alert("hello word");
+//let answer = confirm("Are you here");
+//console.log(answer);
+let answer = prompt("how you age?", "19");
+
+console.log(answer);
