41cd09407d724193d906133fdf4bfe480911307b
Alexsey <dollar123dollar@mail.ru>
2020-03-25 19:05:52 +0200

    popravka

--- star1/source.js
+++ star1/source.js
@@ -5,16 +5,21 @@ let money = prompt("You budget?","1000"),
 
 let appData = {
     money: money,
-    timeData: time
-};
-let expenses = {
-    oneQstens: prompt("stat rashod mesyac","eat"),
-    twoQstens: prompt("skolko?","200")
-};
-let income = [];
+    timeData: time,
+    income: [],
+    sevings: false,
+    expenses: {}
 
-let sevings = false;
+
+};
+let a1 = prompt("stat rashod mesyac","eat"),
+    a2 = prompt("skolko?","200"),
+    a3 = prompt("stat rashod mesyac","eat"),
+    a4 = prompt("skolko?","200");
+
+    appData.expenses.a1 = a2;
+    appData.expenses.a2 = a3;
+
 alert("budget\n" + appData.money/30)
 
 console.log(appData.money);
-
