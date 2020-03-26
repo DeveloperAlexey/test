b400e1ea75e907534cf3a048742df4c800020630
Alexsey <dollar123dollar@mail.ru>
2020-03-26 21:04:57 +0200

    9zd

--- star1/source.js
+++ star1/source.js
@@ -12,14 +12,32 @@ let appData = {
 
 
 };
-let a1 = prompt("stat rashod mesyac","eat"),
-    a2 = prompt("skolko?","200"),
-    a3 = prompt("stat rashod mesyac","eat"),
-    a4 = prompt("skolko?","200");
 
-    appData.expenses.a1 = a2;
-    appData.expenses.a2 = a3;
+
+for (let i = 0; i < 2; i++){
+     let a = +prompt("stat rashod mesyac","eat"),
+         b = +prompt("skolko?","200");
+
+     if ((typeof(a))==='string' && (typeof(a))!= null && (typeof(b))!= null
+          && a != '' && b != '' && a.length < 50){
+              console.log("done");
+              appData.expenses[a] = b;
+};
+};
+    
 
 alert("budget\n" + appData.money/30)
 
-console.log(appData.money);
+if(appData.money/30 < 100){
+    console.log("minimal level");
+}else if(appData.money/30 > 100 && appData.money/30 < 2000){
+    console.log("averedge level");
+}else if(appData.money/30 > 2000){
+    console.log("high level");
+}else{
+    console.log("error");
+}
+
+
+
+
