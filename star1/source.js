9b27e06065fc17c6887b54a9f4e07627b36b1475
Alexsey <dollar123dollar@mail.ru>
2020-03-30 21:07:18 +0200

    10zd

--- star1/source.js
+++ star1/source.js
@@ -1,32 +1,47 @@
 'use strict';
 
-let money = prompt("You budget?","1000"),
-    time = prompt("data","2000-11-23");
+let money, time;
+
+function start(){
+     money = prompt("You budget?","1000");
+     time = prompt("data","2000-11-23");
+     while(isNaN(money) || money == "" || money == null){
+         money = prompt("You budget?","1000");
+
+     }
+}
+
+start();
+
 
 let appData = {
     money: money,
     timeData: time,
     income: [],
-    sevings: false,
+    sevings: true,
     expenses: {}
 
 
 };
 
+function chooseExpenses(){
+    for (let i = 0; i < 2; i++){
+        let a = prompt("stat rashod mesyac","eat"),
+            b = prompt("skolko?","200");
+   
+        if ((typeof(a))==='string' && (typeof(a))!= null && (typeof(b))!= null
+           && a != '' && b != '' && a.length < 50){
+                 console.log("done");
+                 appData.expenses[a] = b;
+        }else{
+            i = i - 1;
+        }
+    }
+}
 
-for (let i = 0; i < 2; i++){
-     let a = +prompt("stat rashod mesyac","eat"),
-         b = +prompt("skolko?","200");
+chooseExpenses();
 
-     if ((typeof(a))==='string' && (typeof(a))!= null && (typeof(b))!= null
-          && a != '' && b != '' && a.length < 50){
-              console.log("done");
-              appData.expenses[a] = b;
-};
-};
-    
-
-alert("budget\n" + appData.money/30)
+alert("budget\n" + (appData.money/30).toFixed());
 
 if(appData.money/30 < 100){
     console.log("minimal level");
@@ -38,6 +53,13 @@ if(appData.money/30 < 100){
     console.log("error");
 }
 
+function checkSavings(){
+    if(appData.sevings == true){
+        let save = +prompt("how many accumulated money"),
+            percent = +prompt("how many percent");
 
-
-
+        appData.monthIncome = save/100/12 + percent;
+        alert("income in month\n" + appData.monthIncome);
+    }
+}
+checkSavings();
