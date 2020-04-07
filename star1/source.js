25891a135f32c5e867d417173e1f238114ceb6a5
Alexsey <dollar123dollar@mail.ru>
2020-04-07 19:46:08 +0200

    5april

--- star1/source.js
+++ star1/source.js
@@ -20,62 +20,68 @@ let appData = {
     income: [],
     sevings: true,
     expenses: {},
-    optionalExpenses:{}
-
-
-};
-
-function chooseExpenses(){
-    for (let i = 0; i < 2; i++){
-        let a = prompt("stat rashod mesyac","eat"),
-            b = prompt("skolko?","200");
-   
-        if ((typeof(a))==='string' && (typeof(a))!= null && (typeof(b))!= null
-           && a != '' && b != '' && a.length < 50){
-                 console.log("done");
-                 appData.expenses[a] = b;
-        }else{
-            i = i - 1;
+    optionalExpenses:{},
+    chooseExpenses:function(){
+        for (let i = 0; i < 2; i++){
+            let a = prompt("stat rashod mesyac","eat"),
+                b = prompt("skolko?","200");
+       
+            if ((typeof(a))==='string' && (typeof(a))!= null && (typeof(b))!= null
+               && a != '' && b != '' && a.length < 50){
+                     console.log("done");
+                     appData.expenses[a] = b;
+            }else{
+                i = i - 1;
+            }
+        }
+    },
+    detectDayBudget:function(){
+        alert("budget\n" + (appData.money/30).toFixed());
+   },
+   detectLevel:function(){
+    if(appData.money/30 < 100){
+        console.log("minimal level");
+    }else if(appData.money/30 > 100 && appData.money/30 < 2000){
+        console.log("averedge level");
+    }else if(appData.money/30 > 2000){
+        console.log("high level");
+    }else{
+        console.log("error");
+    }
+    },
+    ceckSavings:function(){
+        if(appData.sevings == true){
+            let save = +prompt("how many accumulated money"),
+              percent = +prompt("how many percent");
+    
+            appData.monthIncome = save/100/12 + percent;
+            alert("income in month\n" + appData.monthIncome);
+        }
+    },
+    chooseIncome: function(){
+        for(let i = 0; i < 1;i++){
+           let item = prompt("what will bring","");
+           if(typeof(item) === 'string' && item != null && item != ''){
+           appData.income = item.split(', ');
+           appData.income.push(prompt("maybe?"));
+           appData.income.sort();
+           appData.income.forEach(function(item, i , arr){
+               i = i+1;
+               console.log(i+' '+ item)
+           })
+           }else{
+               i = i - 1;
+           }
+        }
+    },
+    cooseOptExpenses:function(){
+        let i;
+        for(i = 1;i < 4;i++){
+            appData.optionalExpenses[i] = prompt("rasohod na kurag");
         }
     }
-}
-
-chooseExpenses();
-
-function detectDayBudget(){
-     alert("budget\n" + (appData.money/30).toFixed());
-}
-
-detectDayBudget();
-function detectLevel(){
-     if(appData.money/30 < 100){
-         console.log("minimal level");
-     }else if(appData.money/30 > 100 && appData.money/30 < 2000){
-         console.log("averedge level");
-     }else if(appData.money/30 > 2000){
-         console.log("high level");
-     }else{
-         console.log("error");
-     }
-}
-
-detectLevel();
-
-function checkSavings(){
-    if(appData.sevings == true){
-        let save = +prompt("how many accumulated money"),
-          percent = +prompt("how many percent");
-
-        appData.monthIncome = save/100/12 + percent;
-        alert("income in month\n" + appData.monthIncome);
-    }
-}
-checkSavings();
-
-function chooseOptExpenses(){
-    let i;
-    for(i = 1;i < 4;i++){
-        appData.optionalExpenses[i] = prompt("rasohod na kurag");
-    }
-}
-chooseOptExpenses();
+};
+console.log("us program have this data");
+for(var key in appData){
+    console.log(key + " ",appData[key]);
+};
